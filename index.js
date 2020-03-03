dcb741b2e895cd6771995792285f851a6c429c5d
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-03-03 10:06:27 +0200

    Fix again lol

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-//let myButton = document.createElement("button");
-//myButton.innerHTML = "Words";
-//let x = document.getElementsByClassName("ClassName")[0];
-//x.appendChild(myButton);
-
-
-
 let myBlogButton = document.getElementsByClassName("ButtonTest")[0];
 myBlogButton.addEventListener("click", function () { location.href = "blogs/My-Blogs.html"; });
