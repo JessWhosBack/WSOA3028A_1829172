d7fc7ebf2f5f81801bd550cd485fa2669c74e529
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-03-10 08:00:05 +0200

    added a test button

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let myBlogButton = document.getElementsByClassName("ButtonTest")[0];
 myBlogButton.addEventListener("click", function () { location.href = "blogs/My-Blogs.html"; });
+
+
+let myBlogButton2 = document.getElementsByClassName("ButtonTest2")[0];
+myBlogButton2.addEventListener("click", function () { location.href = "blogs/My-Blogs.html"; });
