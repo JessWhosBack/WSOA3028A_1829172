7ca3229ce129ce43951a358d8f39a0a51e023244
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-03-19 15:24:29 +0200

    try fix buttons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let myBlogButton = document.getElementsByClassName("ButtonTest")[0];
-myBlogButton.addEventListener("click", function () { location.href = "blogs/My-Blogs.html"; });
+myBlogButton.addEventListener("click", function () { location.href = "blogs/"; });
 
 
-let myBlogButton2 = document.getElementsByClassName("ButtonTest2")[0];
-myBlogButton2.addEventListener("click", function () { location.href = "blogs/My-Blogs.html"; });
+//let myBlogButton2 = document.getElementsByClassName("ButtonTest2")[0];
+//myBlogButton2.addEventListener("click", function () { location.href = "blogs/"; });
