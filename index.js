d672e46933acccc1e51ffd6162abd8541282401c
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-04-22 11:48:07 +0200

    Adding a bit of CSS to header to start learning

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let myBlogButton = document.getElementsByClassName("ButtonTest")[0];
 myBlogButton.addEventListener("click", function () { location.href = "blogs/"; });
 
+let button2_2 = document.getElementsByClassName("button2_2")[0];
+button2_2.addEventListener("click", function () { location.href = "/2020/Week2/Reflection-Alan-Turing/"; });
+
+
 
 //let myBlogButton2 = document.getElementsByClassName("ButtonTest2")[0];
 //myBlogButton2.addEventListener("click", function () { location.href = "blogs/"; });
