1afc78d0da10db2eba20239686ac34d50bdc10fa
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-04-24 15:55:20 +0200

    Make pretty - blog table specifically and BUTTONS

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let myBlogButton = document.getElementsByClassName("ButtonTest")[0];
 if (myBlogButton != null) {
+    myBlogButton.style.cursor = "pointer";
     myBlogButton.addEventListener("click", function () { location.href = "blogs/"; });
 }
 
+let myBlogPageButtons = document.getElementsByClassName("blogPageButton")
+if (myBlogPageButtons != null) {
+    for (let i = 0; i < myBlogPageButtons.length; i++) {
+        myBlogPageButtons[i].style.cursor = "pointer";
+    }
+}
 
 let lastScrollTop = 0;
 let myHeader = document.querySelector("header");
