b8a78e8f2c6e5a60d4b10cc2af63e906d2ee630e
JessWhosBack <60912625+JessWhosBack@users.noreply.github.com>
2020-06-29 12:33:36 +0200

    testing json

--- Learn-With-Me/main.js
+++ Learn-With-Me/main.js
@@ -171,6 +171,7 @@ function displayJSON(data) {
     $('#resultsHeading_JSON').text("The original JSON result:");
     var contents = JSON.stringify(data[0], null, 4);
     $('#results_JSON').text(contents);
+    console.log(contents);
 }
 
 function resetResults() {
